ac70f10ba976a979881f666795611e5ac703baa5
Han Gao <ghnankai@qq.com>
2016-10-08 13:45:50 -0700

    Fix the bug in Longest Substring Without Repeating Characters

--- src/leetcode/LongestSubstringwithoutReapeatCharacter.java
+++ src/leetcode/LongestSubstringwithoutReapeatCharacter.java
@@ -3,29 +3,40 @@ package leetcode;
  * For example, the longest substring without repeating letters for "abcabcbb" is "abc",
  * which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
  */
-/* brute force
+/* TLE: brute force
  */
-import java.util.Hashtable;
+
+import java.util.HashMap;
 
 public class LongestSubstringwithoutReapeatCharacter {
 
-	public int lengthOfLongestSubstring(String s) {
-		char[] str = s.toCharArray();
-		int max = 0;
-		for (int i = 0; i < str.length; i++) {
-			int count = 0;
-			Hashtable<Character, Integer> hash = new Hashtable<Character, Integer>();
-			for (int j = i; j < str.length; j++) {
-				if (!hash.containsKey(str[j])) {
-					hash.put(str[j], j);
-				} else {
-					count = hash.get(str[j]) - i + 1;
-					break;
-				}
-			}
-			if (count > max)
-				max = count;
-		}
-		return max;
-	}
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0)
+            return 0;
+        char[] str = s.toCharArray();
+        int max = 0;
+        for (int i = 0; i < str.length; i++) {
+            int count = 0;
+            HashMap<Character, Integer> hash = new HashMap<Character, Integer>();
+            int j = 0;
+            for (j = i; j < str.length; j++) {
+                if (!hash.containsKey(str[j])) {
+                    hash.put(str[j], j);
+                } else {
+                    count = j - i;
+                    break;
+                }
+            }
+            if (j == str.length)
+                count = str.length - i;
+            if (count > max)
+                max = count;
+        }
+        return max == 0 ? 1 : max;
+    }
+
+    public static void main(String[] args) {
+        String s = "cdd";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
 }
